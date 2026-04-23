f728d9779f6c46aea745db5210dc623c68e5ae50
Glioh <michael.th.luu@gmail.com>
2026-04-23 00:28:24 -0400

    added todo

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,32 +1,32 @@
-import Image from 'next/image';
+import Image from "next/image";
 import { ProjectForm } from "@/modules/home/ui/components/project-form";
 
 const Page = () => {
-  return (
-    <div className="flex flex-col max-w-5xl mx-auto w-full">
-      <section className="space-y-6 py-[16vh] 2xl:py-48">
-        <div className="flex flex-col items-center">
-          <Image
-            src="/logo.svg"
-            alt="Lambda logo"
-            width={50}
-            height={50}
-            className="hidden md:block"
-          />
-        </div>
-        <h1 className="text-2xl md:text-5xl font-bold text-center">
-          Build something with Lambda
-        </h1>
-        <p className="text-lg md:text-xl text-muted-foreground text-center">
-          Create apps and websites by chatting with AI
-        </p>
-        <div className="max-w-3xl mx-auto w-full">
-          <ProjectForm />
-        </div>
-      </section>
-    </div>
-  )
-
-}
+	return (
+		<div className="flex flex-col max-w-5xl mx-auto w-full">
+			<section className="space-y-6 py-[16vh] 2xl:py-48">
+				<div className="flex flex-col items-center">
+					<Image
+						src="/logo.svg"
+						alt="Lambda logo"
+						width={50}
+						height={50}
+						className="hidden md:block"
+					/>
+				</div>
+				<h1 className="text-2xl md:text-5xl font-bold text-center">
+					Build something with Lambda
+				</h1>
+				<p className="text-lg md:text-xl text-muted-foreground text-center">
+					Create apps and websites by chatting with AI
+				</p>
+				<div className="max-w-3xl mx-auto w-full">
+					<ProjectForm />
+				</div>
+			</section>
+			{/* TODO: Add planet visual  */}
+		</div>
+	);
+};
 
 export default Page;
